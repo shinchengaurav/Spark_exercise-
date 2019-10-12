92060a72f0c5f8c800892f917b0ce8dafa9ebdee
shinchengaurav <40812963+shinchengaurav@users.noreply.github.com>
2019-10-12 23:17:34 +0530

    if loop uodate code

--- Exercise_solution.py
+++ Exercise_solution.py
@@ -204,6 +204,20 @@ max_df.select('Year','max(High)').show()
 #####
 ##################################
             # AVERAGE CLOSE FOR EACH CALENDAR MONTH
+  #checking condition if mid value is equal to search value or not
+        if(arr[mid] == x):
+            return mid
+
+        #if not and value is greater than mid value ignore left part and increase its value 
+        elif arr[mid] < x:
+            l = mid+1
+
+
+        # if value less than mid value ignore right part and decrease right value by -1
+        else: 
+            r= mid-1
+       
+    return -1
 
 
 date1 = df.withColumn("Month",month('Date'))
